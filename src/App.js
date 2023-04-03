d14169b9329eec90ac2b5d2707fc497f3d56bebe
1oleg5 <panolegv@gmail.com>
2023-04-03 15:04:03 +0300

    lesson_5_Dogs_and_Cats

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-import React, {useEffect, useState} from "react";
-import Post from "./Components/Users/Post";
-import {getUser, getUsers} from "./Components/Users/getPost";
+import React, {useReducer, useRef} from 'react';
+import css from './App.module.css';
+import Cats from "./components/Cats";
+import Dogs from "./components/Dogs";
 
-function App() {
-    let [users, setUsers] = useState([]);
-    let [chosenUser, setChosenUser] = useState(null);
-    useEffect(() => {
-        getUsers().then(value => setUsers(value));
-    }, [])
+const reduser = (state, action) => {
+    switch (action.type) {
+        case 'ADD_CAT':
+    const [lastCat] = state.cats.slice(-1);
+    const id = lastCat ? lastCat.id + 1 : 0;
+            return {...state, cats:[...state.cats,{id, name:action.payload}]}
+        case 'DELETE_CAT':
+            const index = state.cats.findIndex(cat => cat.id === action.payload);
+            state.cats.splice(index,1)
+            return {...state}
+        case 'ADD_DOG':
+            const [lastDog] = state.dogs.slice(-1);
+            const idDog = lastDog ? lastDog.id + 1 : 0;
+            return {...state, dogs:[...state.dogs,{id:idDog, name:action.payload}]}
+        case 'DELETE_DOG':
+            const indexDog = state.dogs.findIndex(dog => dog.id === action.payload);
+            state.dogs.splice(indexDog,1)
+            return {...state}
+        default:
+            return {...state};
 
-    const choseUser = (id) => {
-        getUser(id).then(value => setChosenUser(value));
+    }
+}
+
+export const App = () => {
+    const catInp = useRef();
+    const dogInp = useRef();
+    const [state, dispatch] = useReducer(reduser,{cats:[], dogs:[]},(data)=>data)
+
+    function createCat() {
+        dispatch({type:'ADD_CAT', payload:catInp.current.value})
+        catInp.current.value = '';
     }
 
-    return (<div>
-            <div>{chosenUser?.body}</div>
-            <hr/>
-            {
-                users.map(value => <Post
-                    key={value.id}
-                    item={value}
-                    choseUser={choseUser}
-                />)
-            }
+    function createDog() {
+        dispatch({type:'ADD_DOG', payload:dogInp.current.value})
+        dogInp.current.value = '';
+    }
 
+    return (
+        <div className={css.App}>
+            <div>
+                <input type="text" ref={catInp}/>
+                <button onClick={createCat}>create_Cat</button>
+                <Cats cats={state.cats} dispatch={dispatch}/>
+            </div>
+            <div>
+                <input type="text" ref={dogInp}/>
+                <button onClick={createDog}>create_Dog</button>
+                <Dogs dogs={state.dogs} dispatch={dispatch}/>
+            </div>
         </div>
     );
 }
-
-export default App;
